871909c05e1b5e753bda1bb2c296c476f9331dd0
Ka Sing Au <kelvinksau@gmail.com>
2025-11-24 16:25:21 +0800

    initial commit

---
+++ agent_engine.py
@@ -0,0 +1,5 @@
+from orchester.agent import root_agent
+
+# This file serves as the entry point for 'adk deploy agent_engine'
+# It exposes the agent object that will be deployed.
+agent = root_agent
